f57f777a8eaf29f164790bc61c6ea0699dd877fb
Beau Elliott <belliott1516@gmail.com>
2022-03-30 12:57:12 -0700

    compeleted create bunnies function in create.js

--- create/create.js
+++ create/create.js
@@ -10,27 +10,36 @@ const logoutButton = document.getElementById('logout');
 
 form.addEventListener('submit', async e => {
     // prevent default
-
+    e.preventDefault();
     // get the name and family id from the form
-
+    const data = new FormData(form);
+    const bunnyName = data.get('bunny-name');
+    const familyID = data.get('family-id');
     // use createBunny to create a bunny with this name and family id
-    
+    createBunny({ 
+        name: bunnyName, 
+        family_id: familyID
+    });
     form.reset();
+    location.replace('./families');
 });
 
-window.addEventListener('load', async() => {
+window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
-
+    const dropdownEl = document.querySelector('select');
     // go get the families from supabase
-
+    const families = await getFamilies();
     // for each family
-
+    for (let family of families){
     // create an option tag
-
+        const optionEl = document.createElement('option'); 
     // set the option's value and text content
-
+        optionEl.value = family.id;
+        optionEl.textContent = family.name;
     // and append the option to the select
+        dropdownEl.append(optionEl);
+    }
 });
 
 
--- families/families.js
+++ families/families.js
@@ -9,10 +9,11 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-function displayFamilies() {
+async function displayFamilies() {
     // fetch families from supabase
-
+    const families = await getFamilies();
     // clear out the familiesEl
+    familiesEl.textContent = '';
 
     for (let family of families) {
         // create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
@@ -24,17 +25,33 @@ function displayFamilies() {
         //        <div class="bunny">Bob</div>
         //    </div>
         // </div>
+        const familyDiv = document.createElement('div');
+        const familyName = document.createElement('h3');
+        const bunnyDiv = document.createElement('div');
         // add the bunnies css class to the bunnies el, and family css class to the family el
+        bunnyDiv.classList.add('bunnies');
+        familyDiv.classList.add('family');
         // put the family name in the name element
+        familyName.textContent = family.name;
         // for each of this family's bunnies
-        //    make an element with the css class 'bunny', and put the bunny's name in the text content
-        //    add an event listener to the bunny el. On click, delete the bunny, then refetch and redisplay all families.
-        // append this bunnyEl to the bunniesEl
+        for (let bunny of family.fuzzy_bunnies){
+            const bunnyNameDiv = document.createElement('div');
+            //make an element with the css class 'bunny', and put the bunny's name in the text content
+            bunnyNameDiv.classList.add('bunny');
+            bunnyNameDiv.textContent = bunny.name;
+            //    add an event listener to the bunny el. On click, delete the bunny, then refetch and redisplay all families.
+            bunnyNameDiv.addEventListener('click', async () => {
+                await deleteBunny();
+                displayFamilies();
+            });
+            // append this bunnyEl to the bunniesEl
+            bunnyDiv.append(bunnyNameDiv);
+        }
+         // append the bunniesEl and nameEl to the familyEl
+        familyDiv.append(familyName, bunnyDiv);
+        // append the familyEl to the familiesEl
+        familiesEl.append(familyDiv);
     }
-
-    // append the bunniesEl and nameEl to the familyEl
-
-    // append the familyEl to the familiesEl
 }
 
 window.addEventListener('load', async () => {
